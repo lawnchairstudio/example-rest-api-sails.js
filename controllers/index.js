f14105b2a0fcdbf2a949d4b6e49697df0da00755
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-17 22:14:54 -0600

    Fuck Kraken
    Developers that don’t write documentation with their code are wasting
    their time.

--- controllers/index.js
+++ controllers/index.js
@@ -7,7 +7,7 @@ module.exports = function (router) {
   var model = new IndexModel();
   
   router.get('/', function (req, res) {
-    res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+    res.render('index', model);
   });
   
 };
--- controllers/signin.js
+++ controllers/signin.js
@@ -7,7 +7,7 @@ module.exports = function (router) {
   var model = new SigninModel();
   
   router.get('/signin', function (req, res) {
-    res.send('<code><pre>' + JSON.stringify(model, null, 2) + '</pre></code>');
+    res.send();
   });
   
 };
