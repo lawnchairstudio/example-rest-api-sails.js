1f0aa5362752c20bfd8898ba58c9587d2d6cc09c
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-17 20:51:27 -0600

    Fix npm start script

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 var app = require('./index');
 var http = require('http');
 
-var server = http.createServer(app);
+var server;
+
+server = http.createServer(app);
 server.listen(process.env.PORT || 8000);
 server.on('listening', function () {
     console.log('Server listening on http://localhost:%d', this.address().port);
