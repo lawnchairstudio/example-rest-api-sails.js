7225349c8e72ca35898dda5b61866715a95835b7
Liam Craver <studios.lime@gmail.com>
2015-01-18 00:04:06 -0600

    Setup structure of user

--- api/models/User.js
+++ api/models/User.js
@@ -7,8 +7,34 @@
 
 module.exports = {
 
+  schema: true,
+
   attributes: {
+    name: {
+      type: "string",
+      required: true
+    },
+    title: {
+      type: "string",
+    },
+    email: {
+      type: "string",
+      email: true,
+      required: true,
+      unique: true
+    },
+    encryptedPassword: {
+      type: "string",
+    }
 
+    /*toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      delete obj.confirmation;
+      delete obj.encryptedPassword;
+      delete obj._csrf;
+      return obj;
+    }*/
   }
-};
 
+};
