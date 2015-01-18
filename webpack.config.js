3b14211f7300cdef37c20058dfb092b791c08544
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-18 16:44:49 -0600

    Add entries to WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,24 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    common: './source/js/common',
-    register: '',
-    profile: '',
+    register: './assets/js/modules/register',
+    profile: './assets/js/modules/profile',
   },
   output: {
-    publicPath: '/public/js/bundles/',
-    path: './dest/public/js/bundles/',
+    publicPath: '/public/bundles/',
+    path: './.tmp/public/bundles/',
     filename:'[name].js'  
   },
   module: {
     loaders: [
+      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.jsx$/, loader: 'jsx-loader?harmony' },
+      { test: /\.jsx$/, exclude: /node_modules/, loader: 'jsx-loader?harmony' },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
     ]
   },
   resolve: {
-    extensions: ['.css','.js','.json'] 
+    extensions: ['.css','.js','.json', '.jsx'] 
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
