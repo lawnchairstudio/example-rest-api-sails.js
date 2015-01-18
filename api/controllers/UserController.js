6f49c0d5b150152461072fcbafb0485f9428ddbd
Liam Craver <studios.lime@gmail.com>
2015-01-18 00:03:55 -0600

    Added create function and new function

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,28 @@
  */
 
 module.exports = {
-	
-};
 
+	'new': function (req, res) {
+		res.view();
+	},
+
+	create: function (req, res, next) {
+
+		// Create a user using the new.ejs sign up form params
+		User.create(req.params.all(), function userCreated (err, user) {
+			// If we have an error creating the user
+			if (err) {
+				console.log(err);
+				req.session.flash = {
+					err: err
+				}
+
+				// redirect back to user signup
+				return res.redirect('/user/new');
+			}
+
+			// Create the user successfully and redirect to the show action
+			res.json(user);
+		});
+	}
+};
