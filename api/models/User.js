e5aecfb177e57827cf571fac42d17d1c59064484
Liam Craver <studios.lime@gmail.com>
2015-01-18 14:27:13 -0600

    Added password encryption

--- api/models/User.js
+++ api/models/User.js
@@ -37,6 +37,19 @@ module.exports = {
       delete obj._csrf;
       return obj;
     }
+  },
+
+  beforeCreate: function (values, next) {
+    if(!values.password || values.password != values.confirmation) {
+      return next({err: ["Password doesn't match password confirmation."]});
+    }
+
+    require('bcrypt').hash(values.password, 10, function passwordEncrypted(err, encryptedPassword) {
+      if(err)
+        return next(err);
+      values.encryptedPassword = encryptedPassword;
+      next();
+    });
   }
 
 };
