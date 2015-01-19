a88a0de69df819dac68ac827aba7a9d96c980565
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-18 21:47:43 -0600

    Update models config

--- config/models.js
+++ config/models.js
@@ -1,6 +1,5 @@
 /**
- * Default model configuration
- * (sails.config.models)
+ * sails.config.models
  *
  * Unless you override them, the following properties will be included
  * in each of your models.
@@ -10,25 +9,6 @@
  */
 
 module.exports.models = {
-
   migrate: 'safe',
-
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  connection: 'mongodb'
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  // migrate: 'alter'
-
+  connection: 'productionMongo'
 };
