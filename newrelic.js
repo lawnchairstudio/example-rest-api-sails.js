aa494b2e564b2f2b3ff8efb2a9112edef0d928ff
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-21 16:27:51 -0600

    Add the fucking NewRelic key

--- newrelic.js
+++ newrelic.js
@@ -5,20 +5,25 @@
  * description of configuration variables and their potential values.
  */
 exports.config = {
+
   /**
    * Array of application names.
    */
-  app_name : ['Segment'],
+  app_name: ['Segment'],
+
   /**
    * Your New Relic license key.
    */
-  license_key : 'license key here',
-  logging : {
+  license_key: '3b27fea0ee497bbeb501cab516c9599f25fe942d',
+
+  logging: {
+
     /**
      * Level at which to log. 'trace' is most useful to New Relic when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level : 'info'
+    level: 'trace'
   }
+
 };
