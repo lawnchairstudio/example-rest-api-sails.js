a67fdf0948e7591e993477b175110dafdcb43b98
Patrick Burtchaell <patrick@pburtchaell.com>
2015-01-21 17:43:54 -0600

    Update newrelic hook to only run in production env

--- api/hooks/newrelic/index.js
+++ api/hooks/newrelic/index.js
@@ -5,7 +5,9 @@ module.exports = function (sails) {
   return {
 
     initialize: function (next) {
-      require('newrelic');
+      if (process.env.NODE_ENV === 'production') {
+        require('newrelic');
+      }
       return next();
     }
 
