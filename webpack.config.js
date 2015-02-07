7351e364bde1895af77c7d3f44a3803287fd87db
Patrick Burtchaell <patrick@pburtchaell.com>
2015-02-06 23:58:24 -0600

    Updatde wekpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,52 @@
+var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'eval-source-map',
+  debug: true,
+  context: path.join(__dirname + '/assets/js/modules'),
   entry: {
-    register: './assets/js/modules/register',
-    profile: './assets/js/modules/profile',
+    profile: [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './profile'
+    ]
   },
   output: {
     publicPath: '/public/bundles/',
-    path: './.tmp/public/bundles/',
-    filename:'[name].js'  
+    path: path.join(__dirname + '/.tmp/public/bundles/'),
+    filename:'[name].js'
+  },
+  externals: {
+    'react': 'react'
   },
   module: {
     loaders: [
-      { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.jsx$/, exclude: /node_modules/, loader: 'jsx-loader?harmony' },
-      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'} // inline base64 URLs for <=8k images, direct URLs for the rest
+      {
+        test: /\.less$/,
+        loader: "style-loader!css-loader!less-loader"
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'jsx-loader?harmony'
+      },
+      {
+        test: /\.(png|jpg)$/, // inline base64 URLs for <=8k images, direct URLs for the rest
+        loader: 'url-loader?limit=8192'
+      }
     ]
   },
   resolve: {
-    extensions: ['.css','.js','.json', '.jsx'] 
+    extensions: ['.css','.js','.jsx','.json'],
+    modulesDirectory: ['web_modules', 'node_modules']
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin({
@@ -30,8 +55,8 @@ module.exports = {
       }
     }),
     new webpack.DefinePlugin({
-      development: JSON.stringify(JSON.parse(process.env.DEVELOPMENT || 'true')),
-      staging: JSON.stringify(JSON.parse(process.env.STAGING || 'false'))
+      _DEVELOPMENT_: JSON.stringify(JSON.parse(process.env.DEVELOPMENT || 'true')),
+      _STAGING_: JSON.stringify(JSON.parse(process.env.STAGING_|| 'false'))
     })
   ]
 
